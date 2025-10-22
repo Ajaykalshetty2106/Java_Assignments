3cc992700f2d1728861a84ccb4150298d4a70d99
Ajaykalshetty2106 <ajaykalshetty2106@gmail.com>
2025-10-22 23:08:40 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class loop {
+    public static void main(String[] args) {
+        
+    
+    for(int i=1;i<=100;i++)
+    {
+        if(i%5==0)
+        continue;
+        System.out.println(i);
+    }
+    System.out.println();
+
+}
+}
