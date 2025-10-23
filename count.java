fc9e92a310556ef1ebba2f898f45d37f17f2a6da
Ajaykalshetty2106 <ajaykalshetty2106@gmail.com>
2025-10-23 09:27:52 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class count {
+    public static void main(String[] args) {
+        int num=4567;
+        int count=0;
+        while(num!=0)
+        {
+            num = num/10;
+            count++;
+        }
+        System.out.println("Number of Digits:" + count);
+
+        
+    }
+
+}
