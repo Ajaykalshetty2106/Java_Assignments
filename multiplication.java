52515ffab60b217cf8f7681b2eca551659add5aa
Ajaykalshetty2106 <ajaykalshetty2106@gmail.com>
2025-10-23 09:27:21 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class multiplication {
+    public static void main(String[] args) {
+        int temp=7;
+        for(int i=1;i<=10;i++)
+        {
+        System.out.println(temp + " x " + i + " = " + (temp * i));
+        }
+    }
+
+}
