adeaec62db92068f45873633e37df9eed0ed70bd
Ajaykalshetty2106 <ajaykalshetty2106@gmail.com>
2025-10-23 09:28:16 +0530

    Add files via upload

---
+++ sumofdigit.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class sumofdigit {
+    public static void main(String[] args) {
+        
+    
+    int num = 0;
+   
+    for(int i=1;i<=4;i++)
+    {
+        num=num+i;
+    }
+    System.out.println("Sum of numbers = " + num);
+}
+
+
+}
